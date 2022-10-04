ba8729ad44377eaf95b38eb34603e2d93faf61b8
Miro Ali Akbar <Miroaliakbar@gmail.com>
2022-10-04 13:01:19 +0200

    Update Py-Holdem.py
    Lade till en kortlek som skapas där alla kort är en tuple och med funktion att kunna slumpa ordningen i kortleken

--- Py-Holdem.py
+++ Py-Holdem.py
@@ -1,1 +1,15 @@
-print ("hello world!")
+import random
+
+Färg = ["Hjärter", "Spader", "Klöver", "Ruter"]
+Valör = list(range(2, 11)) + ["Ess", "Knekt", "Dam", "Kung"]
+
+def make_deck():
+    deck = []
+    for färg in Färg:
+        for valör in Valör:
+            deck.append((färg,str(valör)))
+    return deck
+
+def shuffle(deck):
+    random.shuffle(deck)
+    return deck
