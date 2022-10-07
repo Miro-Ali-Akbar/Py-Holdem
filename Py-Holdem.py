f12ef76de36e8e811982334f8cf4a94b8f25dc0a
Miro Ali Akbar <Miroaliakbar@gmail.com>
2022-10-07 10:14:08 +0200

    Update Py-Holdem.py

--- Py-Holdem.py
+++ Py-Holdem.py
@@ -1,7 +1,7 @@
 import random
 
 Färg = ["Hjärter", "Spader", "Klöver", "Ruter"]
-Valör = list(range(2, 11)) + ["Ess", "Knekt", "Dam", "Kung"]
+Valör = list(range(1, 14))
 
 def make_deck():
     deck = []
@@ -16,3 +16,32 @@ def shuffle(deck):
 
 def get_deck():
     return shuffle(make_deck)
+
+
+# sätta i hopp alla kort till en lista
+def get_all_cards(player_hand, board):
+    temp_list = player_hand + board
+    return temp_list
+
+# returnerar lista med alla siffror från en hand
+def numbers_hand_list(temp_list):
+    hand_list = []
+    for x in range (0,len(temp_list)):
+        hand_list.append(temp_list[x][1])
+    return hand_list
+
+# räknar antal samma kort i handen
+def count_hand(hand_list):
+    card_list = 13*[0]
+    for x in range (0, len(hand_list)):
+        card_list[hand_list[x]-1] += 1
+    return card_list
+
+# sätter ihopp alla de övre funtionerna, tar in listorna handen och board returnar lista med antal av varje värde
+def get_counted_hand(player_hand, board):
+    return count_hand(numbers_hand_list(get_all_cards(player_hand, board)))
+
+
+hand = [("dum", 1), ("hell", 1)]
+board = [("hello", 1)]
+print(get_counted_hand(hand, board))
