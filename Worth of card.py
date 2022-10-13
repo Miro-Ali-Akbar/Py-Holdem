a5aac1dd9e6610be86e98cfa307b296e5cd6b034
Miro Ali Akbar <Miroaliakbar@gmail.com>
2022-10-13 16:26:09 +0200

    la till allt

--- Worth of card.py
+++ Worth of card.py
@@ -1,23 +1,3 @@
-import random
-
-Färg = ["Hjärter", "Spader", "Klöver", "Ruter"]
-Valör = list(range(1, 14))
-
-def make_deck():
-    deck = []
-    for färg in Färg:
-        for valör in Valör:
-            deck.append((färg,str(valör)))
-    return deck
-
-def shuffle(deck):
-    random.shuffle(deck)
-    return deck
-
-def get_deck():
-    return shuffle(make_deck)
-
-
 # sätta i hopp alla kort till en lista
 def get_all_cards(player_hand, board):
     temp_list = player_hand + board
@@ -227,17 +207,3 @@ def best_hand(hand1, hand2, board):
         return hand1
     else:
         return hand2
-    
-    
-hand = [("Klöver", 9), ("Klöver", 5), ("Hjärter", 13), ("Hjärter", 12), ("Hjärter", 10)]
-hand2 =[("Spader", 3), ("Hjärter", 3), ("Klöver", 2), ("Hjärter", 2),("Ruter", 2), ("Klöver", 3), ("Ruter", 3)]
-board = [("Klöver", 7)]
-print(get_counted_hand(hand, board))
-print(multiple_cards(hand, board))
-print(get_counted_hand(hand2, board))
-print(multiple_cards(hand2, board))
-print(get_color_hand(hand, board))
-print(numbers_color_list(hand))
-print(is_straight(hand, board))
-print(hand_worth(hand, board))
-print(hand_worth(hand2, board))
