f13c9c3fbdd470888fec19eff5ea8bb2eb29d5e9
Miro Ali Akbar <Miroaliakbar@gmail.com>
2022-10-19 08:36:20 +0200

    Revert "Merge branch 'main' of https://github.com/Miro-Ali-Akbar/Py-Holdem"
    This reverts commit 5a5913b20705ed91bebd54cd6d5af8bed9450a34, reversing
    changes made to e54ebe8aa8b01a1b69c461a9c6b5d830ae1782b0.

--- Py-Holdem.py
+++ Py-Holdem.py
@@ -1,29 +1,57 @@
 import random
 from Worth_of_card import *
 
+# spelare står som Player_tal eg Player_1, Player_2
+# total antal spelare står som playerNmbr (2-10)
+
+
 Färg = ["Hjärter", "Spader", "Klöver", "Ruter"]
 Valör = list(range(1, 14))
 
+# gör en dictionary som har {Player_1 : money_max, ... till Player_playerNmbr : money_max}
+def make_money_dictionary(playerNmber, money_max):
+    list_of_money = {}
+    for x in range (1, playerNmber+1):
+        player = "Player_" +str(x)
+        list_of_money[player] = money_max
+    return list_of_money
 
+# gör en lista av alla spelare
+def make_player_list(playerNmber):
+    player_list = []
+    for x in range (1, playerNmber+1):
+        player = "Player_" +str(x)
+        player_list.append(player)
+    return player_list
+    
+    
+# ändar värdet på money för key, player från dictionarien
+def change_money(list_of_money, player, money):
+    temp_money = list_of_money[player]
+    list_of_money[player] = temp_money + money
+
+# Roterar en lista för att gå runt i betting
+def rotate_order_list(player_list):
+    player_list.append(playerlist.pop(0))
+
+# gör en deck med alla kort
 def make_deck():
     deck = []
     for färg in Färg:
         for valör in Valör:
-            deck.append((färg, str(valör)))
+            deck.append((färg,str(valör)))
     return deck
 
-
+# tar en deck och blandar den
 def shuffle(deck):
     random.shuffle(deck)
     return deck
 
-
+# ger en lista som ett deck varav korten är blandade. 
 def get_deck():
     return shuffle(make_deck)
-
-
 hand = [("Klöver", 9), ("Klöver", 2), ("Hjärter", 5), ("Hjärter", 7), ("Hjärter", 10)]
-hand2 = [("Spader", 6), ("Hjärter", 13), ("Klöver", 2), ("Hjärter", 1), ("Ruter", 5), ("Klöver", 9), ("Ruter", 12)]
+hand2 =[("Spader", 6), ("Hjärter", 13), ("Klöver", 2), ("Hjärter", 1),("Ruter", 5), ("Klöver", 9), ("Ruter", 12)]
 board = [("Klöver", 8)]
 print(get_counted_hand(hand, board))
 print(multiple_cards(hand, board))
--- Worth_of_card.py
+++ Worth_of_card.py
@@ -3,11 +3,10 @@ def get_all_cards(player_hand, board):
     temp_list = player_hand + board
     return temp_list
 
-
 # returnerar lista med alla siffror från en hand
 def numbers_hand_list(temp_list):
     hand_list = []
-    for x in range(0, len(temp_list)):
+    for x in range (0,len(temp_list)):
         hand_list.append(temp_list[x][1])
     return hand_list
 
@@ -18,214 +17,199 @@ def count_hand(hand_list):
         card_list[hand_list[x]-1] += 1
     return card_list
 
-
 # sätter ihopp alla de övre funtionerna, tar in listorna handen och board returnar lista med antal av varje värde
 def get_counted_hand(player_hand, board):
-	return count_hand(numbers_hand_list(get_all_cards(player_hand, board)))
-
+    return count_hand(numbers_hand_list(get_all_cards(player_hand, board)))
 
 # returnerar lista med alla färger från en hand
 def numbers_color_list(temp_list):
-	hand_list = []
-	for x in range(0, len(temp_list)):
-		hand_list.append(temp_list[x][0])
-	return hand_list
-
+    hand_list = []
+    for x in range (0,len(temp_list)):
+        hand_list.append(temp_list[x][0])
+    return hand_list
 
 # räknar alla färger
 # returnerar en lista med antal av varje färg [Hjärter, Spader, Klöver, Ruter]
 def color_hand(hand_list):
-	card_list = 4 * [0]
-	for x in hand_list:
-		if x == "Hjärter":
-			card_list[0] += 1
-		elif x == "Spader":
-			card_list[1] += 1
-		elif x == "Klöver":
-			card_list[2] += 1
-		elif x == "Ruter":
-			card_list[3] += 1
-	return card_list
-
+    card_list = 4*[0]
+    for x in hand_list:
+        if x == "Hjärter":
+            card_list[0] += 1
+        elif x == "Spader":
+            card_list[1] += 1
+        elif x == "Klöver":
+            card_list[2] += 1
+        elif x == "Ruter":
+            card_list[3] += 1
+    return card_list
 
 # sätter ihopp alla de övre funtionerna, tar in listorna handen och board returnar lista med antal av varje färg
 def get_color_hand(player_hand, board):
-	return color_hand(numbers_color_list(get_all_cards(player_hand, board)))
+    return color_hand(numbers_color_list(get_all_cards(player_hand, board)))
 
-
-# kollar om handen har fyrtal, kåk, triss dubbelpar eller par
+#kollar om handen har fyrtal, kåk, triss dubbelpar eller par
 def multiple_cards(player_hand, board):
-	temp_list = get_counted_hand(player_hand, board)
-	for x in range(0, len(temp_list)):
-		if temp_list[x] == 4:
-			return "fyrtal"
-	for x in range(0, len(temp_list)):
-		if temp_list[x] == 3:
-			for x in range(0, len(temp_list)):
-				if temp_list[x] == 2:
-					return "kåk"
-			return "triss"
-	for x in range(0, len(temp_list)):
-		if temp_list[x] == 2:
-			for y in range(0, len(temp_list)):
-				if temp_list[y] == 2 and x != y:
-					return "dubbelpar"
-			return "par"
-
+    temp_list = get_counted_hand(player_hand, board)
+    for x in range (0, len(temp_list)):
+        if temp_list[x] == 4:
+            return "fyrtal"
+    for x in range (0, len(temp_list)):
+        if temp_list[x] == 3:
+            for x in range (0, len(temp_list)):
+                if temp_list[x] == 2:
+                    return "kåk"
+            return "triss"
+    for x in range (0, len(temp_list)):
+        if temp_list[x] == 2:
+            for y in range (0, len(temp_list)):
+                if temp_list[y] == 2 and x != y:
+                    return "dubbelpar"
+            return "par"
 
 # Tar två händer som båda har fyrtal, kåk, triss eller par och bestämmer vem som har bäst
 # returnerar true om hand 1 är en bättre hand än två
 def if_same_amount(hand1, hand2, board):
-	first_player = get_counted_hand(hand1, board)
-	second_player = get_counted_hand(hand2, board)
-	# första handens värde
-	hand = multiple_cards(hand1, board)
-	if hand == "fyrtal":
-		best_hand1 = first_player.index(4)
-	elif hand == "kåk":
-		best_hand1 = first_player.index(3)
-	elif hand == "triss":
-		best_hand1 = first_player.index(3)
-	elif hand == "par":
-		best_hand1 = first_player.index(2)
-	# andra handens bästa värde
-	second_hand = multiple_cards(hand2, board)
-	if second_hand == "fyrtal":
-		best_hand2 = second_player.index(4)
-	elif second_hand == "kåk":
-		best_hand2 = second_player.index(3)
-	elif second_hand == "triss":
-		best_hand2 = second_player.index(3)
-	elif second_hand == "par":
-		best_hand2 = second_player.index(2)
-	# får vilken valör som var högst för varje hand
-	if best_hand1 == best_hand2:
-		return None
-	elif best_hand1 == 0:
-		return hand1
-	elif best_hand2 == 0:
-		return hand2
-	elif best_hand1 > best_hand2:
-		return hand1
-	else:
-		return hand2
+    first_player = get_counted_hand(hand1, board)
+    second_player = get_counted_hand(hand2, board)
+# första handens värde
+    hand = multiple_cards(hand1, board)
+    if hand == "fyrtal":
+        best_hand1 = first_player.index(4)
+    elif hand == "kåk":
+        best_hand1 = first_player.index(3)
+    elif hand == "triss":
+        best_hand1 = first_player.index(3)
+    elif hand == "par":
+        best_hand1 = first_player.index(2)
+# andra handens bästa värde
+    second_hand = multiple_cards(hand2, board)
+    if second_hand == "fyrtal":
+        best_hand2 = second_player.index(4)
+    elif second_hand == "kåk":
+        best_hand2 = second_player.index(3)
+    elif second_hand == "triss":
+        best_hand2 = second_player.index(3)
+    elif second_hand == "par":
+        best_hand2 = second_player.index(2)
+# får vilken valör som var högst för varje hand
+    if best_hand1 == best_hand2:
+        return []
+    elif best_hand1 == 0:
+        return hand1
+    elif best_hand2 == 0:
+        return hand2
+    elif best_hand1 > best_hand2:
+        return hand1
+    else:
+        return hand2
 
-
-# Royal flush returnerar True om det är en Royal flush
-def is_royal_flush(hand, board):
-	all_color = False
-	all_cards = False
-	royal_flush = False
-	right_color = 4
-	number_to_color = ["Hjärter", "Spader", "Klöver", "Ruter"]
-	temp_list_color = (get_color_hand(hand, board))
-	# lista med färger
-	temp_list = get_all_cards(hand, board)
-	# lista med alla kort
-	for x in range(0, len(temp_list_color)):
-		if temp_list_color[x] >= 5:
-			all_color = True
-			right_color = x
-	if right_color < 4:
-		right_color = number_to_color[right_color]
-		if all_color:
-			all_cards = (right_color, 1) in temp_list and (right_color, 13) in temp_list and (
-				right_color, 12) in temp_list and (right_color, 11) in temp_list and (right_color, 10) in temp_list
-	royal_flush = all_cards and all_color
-	return royal_flush
-
+# Rolyal flush returnerar True om det är en Royal flush
+def is_royal_flush(hand,board):
+    all_color = False
+    all_cards = False
+    royal_flush = False
+    right_color = 4
+    number_to_color = ["Hjärter", "Spader", "Klöver", "Ruter"]
+    temp_list_color = (get_color_hand(hand, board))
+# lista med färger
+    temp_list = get_all_cards(hand, board)
+# lista med alla kort
+    for x in range (0, len(temp_list_color)):
+        if temp_list_color[x] >= 5:
+            all_color = True
+            right_color = x
+    if right_color < 4:
+        right_color = number_to_color[right_color]
+        if all_color:
+            all_cards = (right_color, 1) in temp_list and (right_color, 13) in temp_list and (right_color, 12) in temp_list and (right_color, 11) in temp_list and (right_color, 10) in temp_list
+    royal_flush = all_cards and all_color
+    return royal_flush
 
 # returnerar högsta valören som handen har om den inte har något annat
 def highest_card(hand, board):
-	temp_hand = get_counted_hand(hand, board)
-	temp_tal = temp_hand.pop(0)
-	temp_hand.append(temp_tal)
-	for x in range(len(temp_hand) - 1, 0, -1):
-		if temp_hand[x] == 1:
-			return x + 2
-
+    temp_hand = get_counted_hand(hand, board)
+    temp_tal = temp_hand.pop(0)
+    temp_hand.append(temp_tal)
+    for x in range (len(temp_hand)-1, 0, -1):
+        if temp_hand[x] == 1:
+            return x+2
 
 # returnerar True om det är en straight flush
 def is_straight_flush(hand, board):
-	all_color = False
-	all_cards = False
-	straight_flush = False
-	right_color = 4
-	number_to_color = ["Hjärter", "Spader", "Klöver", "Ruter"]
-	temp_list_color = (get_color_hand(hand, board))
-	# lista med färger
-	temp_list = get_all_cards(hand, board)
-	# lista med alla kort
-	for x in range(0, len(temp_list_color)):
-		if temp_list_color[x] >= 5:
-			all_color = True
-			right_color = x
-	if right_color < 4:
-		right_color = number_to_color[right_color]
-		if all_color:
-			for x in range(1, 10):
-				if (right_color, x) in temp_list and (right_color, x + 1) in temp_list and (
-						right_color, x + 2) in temp_list and (right_color, x + 3) in temp_list and (
-						right_color, x + 4) in temp_list:
-					all_cards = True
-	straight_flush = all_cards and all_color
-	return straight_flush
-
+    all_color = False
+    all_cards = False
+    straight_flush = False
+    right_color = 4
+    number_to_color = ["Hjärter", "Spader", "Klöver", "Ruter"]
+    temp_list_color = (get_color_hand(hand, board))
+# lista med färger
+    temp_list = get_all_cards(hand, board)
+# lista med alla kort
+    for x in range (0, len(temp_list_color)):
+        if temp_list_color[x] >= 5:
+            all_color = True
+            right_color = x
+    if right_color < 4:
+        right_color = number_to_color[right_color]
+        if all_color:
+            for x in range (1,10):
+                if (right_color, x) in temp_list and (right_color, x+1) in temp_list and (right_color, x+2) in temp_list and (right_color, x+3) in temp_list and (right_color, x+4) in temp_list:
+                    all_cards = True   
+    straight_flush = all_cards and all_color
+    return straight_flush
 
 # returnerar True om det är en flush
 def is_flush(hand, board):
-	flush = False
-	temp_list_color = (get_color_hand(hand, board))
-	# lista med färger
-	for x in range(0, len(temp_list_color)):
-		if temp_list_color[x] >= 5:
-			flush = True
-	return flush
+    flush = False
+    temp_list_color = (get_color_hand(hand, board))
+# lista med färger
+    for x in range (0, len(temp_list_color)):
+        if temp_list_color[x] >= 5:
+            flush = True
+    return flush
 
 # returnerar True om det är en straight
 def is_straight(hand, board):
-	straight = False
-	temp_list = numbers_hand_list(get_all_cards(hand, board))
-	for x in range(1, 10):
-		if x in temp_list and x + 1 in temp_list and x + 2 in temp_list and x + 3 in temp_list and x + 4 in temp_list:
-			straight = True
-	return straight
-
+    straight = False
+    temp_list = numbers_hand_list(get_all_cards(hand, board))
+    for x in range (1,10):
+        if x in temp_list and x+1 in temp_list and x+2 in temp_list and x+3 in temp_list and x+4 in temp_list:
+            straight = True
+    return straight
 
 # returnerar handens värde - lågt är bra
 def hand_worth(hand, board):
-	card = multiple_cards(hand, board)
-	if is_royal_flush(hand, board):
-		worth = 1
-	elif is_straight_flush(hand, board):
-		worth = 2
-	elif card == "fyrtal":
-		worth = 3
-	elif card == "kåk":
-		worth = 4
-	elif is_flush(hand, board):
-		worth = 5
-	elif is_straight(hand, board):
-		worth = 6
-	elif card == "triss":
-		worth = 7
-	elif card == "dubbelpar":
-		worth = 8
-	elif card == "par":
-		worth = 9
-	else:
-		worth = 25 - highest_card(hand, board)
-	return worth
+    card = multiple_cards(hand, board)
+    if is_royal_flush(hand,board):
+        worth = 1
+    elif is_straight_flush(hand, board):
+        worth = 2
+    elif card == "fyrtal":
+        worth = 3
+    elif card == "kåk":
+        worth = 4
+    elif is_flush(hand, board):
+        worth = 5
+    elif is_straight(hand, board):
+        worth = 6
+    elif card == "triss":
+        worth = 7
+    elif card == "dubbelpar":
+        worth = 8
+    elif card == "par":
+        worth = 9
+    else:
+        worth = 25 - highest_card(hand, board)
+    return worth
 
-
-# returnerar handen som är best None om ingen hand är bättre än den andra
+# returnerar handen som är best tom lista om ingen hand är bättre än den andra
 def best_hand(hand1, hand2, board):
-	temp_hand_list = [3, 4, 7, 8]
-	if hand_worth(hand1, board) < hand_worth(hand2, board):
-		return hand1
-	elif hand_worth(hand1, board) > hand_worth(hand2, board):
-		return hand2
-	elif hand_worth(hand1, board) in temp_hand_list and hand_worth(hand1, board) == hand_worth(hand2, board):
-		return if_same_amount(hand1, hand2, board)
-	else:
-		return None
+    temp_hand_list = [3, 4, 7, 8]
+    if hand_worth(hand1, board) < hand_worth(hand2, board):
+        return hand1
+    elif hand_worth(hand1, board) > hand_worth(hand2, board):
+        return hand2
+    elif hand_worth(hand1, board) in temp_hand_list and hand_worth(hand1, board) == hand_worth(hand2, board):
+        return if_same_amount(hand1, hand2, board)
+    else:
+        return []
